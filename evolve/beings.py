10002a576dcad080d12100bec909398cc46c1d7b
root <root@calledit.com>
2015-04-06 17:52:57 -0400

    fixed a lot of movement and hunt logic.

--- evolve/beings.py
+++ evolve/beings.py
@@ -4,11 +4,12 @@ from time import sleep
 from threading import Thread
 #for debug
 import pdb
+from Tkinter import END
 
 directionMap = {i:x for i,x in enumerate("N S E W".split())}
 
 class Beings:
-    def __init__(self, world, predCount, preyCount, sbtn):
+    def __init__(self, world, predCount, preyCount, sbtn, bbox):
         '''args world (eg Canvas object), predCount and preyCount, initiates life process when live is called in pain loop
         '''
         self.world = world
@@ -20,8 +21,10 @@ class Beings:
         self.beings = list(self.predators + self.prey)
         self.tags = list()
 
-        #stop button for binding after live() is called 
+        #stop button and being box for binding after live() is called 
         self.sbtn = sbtn
+        self.bbox = bbox
+
 
     def live(self):
         self.world.state['state'] = 'active'
@@ -31,7 +34,7 @@ class Beings:
                 being.tag = self.world.create_rectangle(being.genX, being.genY, being.genX+10, being.genY-10, fill=being.color)
             else:
                 being.tag = self.world.create_oval(being.genX, being.genY, being.genX+10, being.genY-10, fill=being.color)
-        
+
         self.session = Thread(target=self.activate)
         self.session.start()
         
@@ -47,47 +50,48 @@ class Beings:
             for pred in self.predators:
                 pred.hunt(self.prey)
                 pred.kill(self.world, self.prey, self.beings) 
+            
+            self.bbox.delete(1.0, END)
+            bboxstr = "Predators \n"
+            for pred in self.predators:
+                bboxstr += "tag-%s; speed-%s; vR-%s; kR-%s; kC-%s; state-%s \n" % (pred.tag, pred.speed, pred.visionRange, pred.killRange, pred.killCount, pred.state)
+            self.bbox.insert("1.0", bboxstr)
             sleep(.3)
-
+            
     def move(self, being):
         '''
         Receives a predator or prey object and gives it a new set of position attributes
         '''
         #if N, decrease Y by speed coef and ensure being isn't above N world boundary
         if directionMap[being.direction] == 'N':
-            thisC = being.genY
+            being.curY = self.world.coords(being.tag)[1]
             being.offset = -1 * (being.speed * .5)
-            being.genY = being.genY - being.offset
-            if being.genY < 20:
-                being.offset = 0 # being.genY + (being.genY - self.world.winfo_height())
-                being.genY = thisC # - being.offset
+            if being.curY < 35:
+                being.offset = -2 # being.genY + (being.genY - self.world.winfo_height())
         #if S, increase Y by speed coef and check to see if it is below the bottom world boudary 
         elif directionMap[being.direction] == 'S':
-            thisC = being.genY
+            being.curY = self.world.coords(being.tag)[1]
             being.offset = being.speed * .5
-            being.genY = being.genY + being.offset
-            if being.genY > self.world.winfo_height():
+            if being.genY > self.world.winfo_height()-50:
                 being.offset = 0 #being.genY - (self.world.winfo_height()-being.genY)
-                being.genY = thisC + being.offset
         #if E decrease X by speed coef and ensure not outside E world boundary
         elif directionMap[being.direction] == 'E':
-            thisC = being.genX
+            being.curX = self.world.coords(being.tag)[0]
             being.offset = being.speed * .5
-            being.genX = being.genX + being.offset
-            if being.genX > self.world.winfo_width():
+            if being.curX > self.world.winfo_width()-50:
                 being.offset = 0
-                being.genX = thisC + being.offset
         #if W increase X by speed coef and ensure not outside W world boundary
         elif directionMap[being.direction] == 'W':
-            thisC = being.genX
+            being.curX = self.world.coords(being.tag)[0]
             being.offset = -1 * (being.speed * .5)
-            being.genX = being.genX - being.offset
             if being.genX < 20:
                 being.offset = 0 # being.genX + (being.genX - self.world.winfo_width())
-                being.genX = thisC# - being.offset
 
-        #randomize being vector
-        being.direction = np.random.randint(4)
+        #randomize being vector if idle
+        if being.state == 'idle':
+            being.direction = np.random.randint(4)
+        if being.type == 'prey':
+            being.direction = np.random.randint(4)
 
         #move the being to the new coords
         if directionMap[being.direction] in ['N','S']:
@@ -111,47 +115,72 @@ class Predator(object):
                         hunt, reproduce methods.
         '''
         self.type = 'predator'
+        self.state = 'idle'
         self.generation = 1
         self.speed = np.random.randint(1,11)
-        self.stamina = np.random.randint(11)
-        self.lifespan = np.random.randint(501)
+        self.stamina = np.random.randint(1, 11)
+        self.lifespan = np.random.randint(100, 501)
         self.state = {'state':'idle'}
         self.direction = np.random.randint(4)
-        self.visionRange = np.random.randint(31)
-        self.strength = np.random.randint(11)
-        self.killRange = np.random.randint(11)
+        self.visionRange = np.random.randint(31, 61)
+        self.strength = np.random.randint(1, 11)
+        self.killRange = np.random.randint(11,21)
         self.genX = np.random.randint(300)
         self.genY = np.random.randint(300)
+        self.curX = self.genX
+        self.curY = self.genY
         self.color = '#'+''.join([np.random.choice('0 1 2 3 4 5 6 7 8 9 A B C D E F'.split()) for x in range(6)])
-        
+        self.killCount = 0 
+        self.target = None
+
     def hunt(self, beings):
         '''
             the 'beings' are actually just prey
         '''
-        for being in beings:
-            if self.genX in range(int(being.genX - self.visionRange), int(being.genX + self.visionRange)) and self.genY in range(int(being.genY - self.visionRange), int(being.genY + self.visionRange)):
-                #if more east, move west
-                if self.genX > being.genX:
-                    self.direction = 3
-                #if west, move east
-                elif self.genX < being.genX:
-                    self.direction = 2
-                #if north, move south
-                elif self.genY > being.genY:
-                    self.direction = 1
-                #if south, move north
-                elif self.genY < being.genY:
-                    self.direction = 0
-                print '%s is hunting %s!!' % (self.tag, being.tag)
+        #if we have a target and the target is still in range
+        if self.target and self.target in range(int(self.target.curX - self.visionRange), int(self.target.curX + self.visionRange)) and self.curY in range(int(self.target.curY - self.visionRange), int(self.target.curY + self.visionRange)):
+            self.navToTarget(self.target)
+        #if there isn't a target
+        else:
+            #try and find a target
+            for being in beings:
+                if self.curX in range(int(being.curX - self.visionRange), int(being.curX + self.visionRange)) and self.curY in range(int(being.curY - self.visionRange), int(being.curY + self.visionRange)):
+                    self.state = 'hunting'
+                    self.target = being
+                    self.navToTarget(being)
+                #no target, stay idle
+                else:
+                    self.state='idle'
+                    self.target=None
+
+    def navToTarget(self, being):
+        #if more east, move west
+        if self.curX < (being.curX-self.killRange) and self.curX < (being.curX+self.killRange):
+            self.direction = 3
+            return
+        #if west, move east
+        elif self.curX > (being.curX-self.killRange) and self.curX > (being.curX+self.killRange):
+            self.direction = 2
+            return
+        #if north, move south
+        elif self.curX < (being.curX-self.killRange) and self.curY < (being.curX+self.killRange):
+            self.direction = 1
+            return
+        #if south, move north
+        elif self.curX > (being.curX-self.killRange) and self.curY < (being.curX+self.killRange):
+            self.direction = 0
+            return
 
     def kill(self, world, prey, allBeings):
         for p in prey:
-            if self.genX in range(int(p.genX - self.killRange), int(p.genX + self.killRange)) and self.genY in range(int(p.genY - self.killRange), int(p.genY + self.killRange)):
-                victims = world.find_overlapping(self.genX-self.killRange, self.genY-self.killRange, self.genX+self.killRange, self.genY+self.killRange)
-                pdb.set_trace()
-                lambda x: world.delete(x), victims
-                lambda x: allBeings.pop(allBeings.index(x)), victims
-                print '%s just KILLED %s!!!!' % (self.tag, p.tag)
+            if self.curX in range(int(p.curX - self.killRange), int(p.curX + self.killRange)) and self.curY in range(int(p.curY - self.killRange), int(p.curY + self.killRange)):
+                try:
+                    allBeings.pop(allBeings.index(p))
+                    prey.pop(prey.index(p))
+                    world.delete(p.tag)
+                    self.killCount += 1
+                    print '%s just KILLED %s!!!!' % (self.tag, p.tag)
+                except: pass
 
 class Prey(object):
     def __init__(self):
@@ -161,16 +190,19 @@ class Prey(object):
 
         '''
         self.type = 'prey'
+        self.state = 'idle'
         self.generation = 1
-        self.speed = np.random.randint(11)
-        self.stamina = np.random.randint(11)
-        self.lifespan = np.random.randint(501)
-        self.visionRange = np.random.randint(11)
-        self.strength = np.random.randint(11)
+        self.speed = np.random.randint(1, 11)
+        self.stamina = np.random.randint(1, 11)
+        self.lifespan = np.random.randint(100, 501)
+        self.visionRange = np.random.randint(1, 11)
+        self.strength = np.random.randint(1, 11)
         self.state = {'state':'idle'}
         self.direction = np.random.randint(4)
         self.genX = np.random.randint(300)
         self.genY = np.random.randint(300)
+        self.curX = self.genX
+        self.curY = self.genY
         self.color = '#'+''.join([np.random.choice('0 1 2 3 4 5 6 7 8 9 A B C D E F'.split()) for x in range(6)])
 
         
--- evolve/evolution.py
+++ evolve/evolution.py
@@ -29,7 +29,7 @@ class Evolution(Frame):
         self.columnconfigure(1, weight=1)
         self.columnconfigure(3, pad=7)
         self.rowconfigure(3, weight=1)
-        self.rowconfigure(5, pad=7)
+        self.rowconfigure(5, pad=30)
 
         lbl = Label(self, text="PyLife")
         lbl.grid(sticky=W, pady=5, padx=25)
@@ -38,20 +38,22 @@ class Evolution(Frame):
         world.grid(row=1, column=0, columnspan=2, rowspan=4, padx=5, sticky=E+W+N+S)
         
         sbtn = Button(self, text="Stop")
-        sbtn.grid(row=2, column=3)
+        sbtn.grid(row=2, column=3, pady = 5)
 
-        abtn = Button(self, text="Start", command=Beings(world, 7, 9, sbtn).live)
-        abtn.grid(row=1, column=3, pady=5)
+        bbox = Text(self, height = 20, width = 20)
+        bbox.grid(row=3, column = 3, rowspan=2, pady=5)
 
         cbtn = Button(self, text="Close", command=self.parent.destroy)
-        cbtn.grid(row=5, column=3, pady=4)
+        cbtn.grid(row=5, column=3, pady=5)
 
         hbtn = Button(self, text="Configure Universe")
         hbtn.grid(row=5, column=0, padx=5)
 
-        
         wbtn = Button(self, text="Step")
         wbtn.grid(row=3, column=3, sticky=N)
+        
+        abtn = Button(self, text="Start", command=Beings(world, 8, 50, sbtn, bbox).live)
+        abtn.grid(row=1, column=3, pady=5)
 
     def close(self):
         self.parent.destroy
